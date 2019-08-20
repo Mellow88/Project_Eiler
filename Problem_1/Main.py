a47bbc400e28ff01fdbb1f2826db0ef246a396b6
Sereda Ihor <seredaihor@ukr.net>
2019-08-20 16:36:05 +0300

    Problem_1 complete

--- Problem_1/Main.py
+++ Problem_1/Main.py
@@ -1,6 +1,9 @@
-import numpy as np
+def get_natural_sum(start, end, arg_1, arg_2):
+    sum_list = []
+    for start in range(end):
+        if start % arg_1 == 0 or start % arg_2 == 0:
+            sum_list.append(start)
+    return sum(sum_list)
 
-def get_natural_sum(arg_1, arg_2):
-    return arg_1 + arg_2
+print(get_natural_sum(1, 1000, 3, 5))
 
-print (1)
